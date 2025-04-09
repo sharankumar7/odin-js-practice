26b196daf8a3195695d923f988edffd149dfd71e
Sharan kumar <bssharankumar28920@gmail.com>
2025-04-09 13:15:36 +0530

    practiceed

---
+++ first/index.js
@@ -0,0 +1,37 @@
+// variables
+// let and const
+
+// let a = 10;
+// let b = "kumar";
+// console.log(a + " " + b);
+// console.log((4 + 6 + 9) / 77);
+
+// functions
+
+// function favoriteAnimal(animal) {
+//   return animal + " is my favorite animal!";
+// }
+
+// console.log(favoriteAnimal("Goat"));
+
+// function favoriteAnimal(animal) {
+//   console.log(animal + " is my favorite animal!");
+// }
+
+// favoriteAnimal("Lion");
+
+// function add7(a) {
+//   return a + 7;
+// }
+
+// console.log(add7(10));
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+// console.log(multiply(2, 3));
+
+// function capitalizeFirstLetter(str) {
+//   return str[0].toUpperCase() + str.slice(1);
+// }
+// console.log(capitalizeFirstLetter("nigga "));
